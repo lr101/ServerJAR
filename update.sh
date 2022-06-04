9ee261978cec7526f4aedac9a507eb6987b8a8ce
Lukas <Lukasr101@gmail.com>
2022-06-04 16:30:06 +0200

    updateDatabase jar

--- update.sh
+++ update.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 git pull
-
-### changes to db ###
-
-
-### ------------- ###
+java -cp updateDatabase -pull='true'
 sudo systemctl restart server.service
+java -cp updateDatabase -pull='false'
